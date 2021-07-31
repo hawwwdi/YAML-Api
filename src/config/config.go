06d37da6e9ebd2a123d50ed2c80f559dc10b2e88
hawwwdi <hawwwdi@gmail.com>
2021-07-31 18:54:36 +0430

    add config package

---
+++ src/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	Port int `json:"port"`
+}
+
+func LoadConfig(path string) Config {
+	handle, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var config Config
+	err = json.Unmarshal(handle, &config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return config
+}
